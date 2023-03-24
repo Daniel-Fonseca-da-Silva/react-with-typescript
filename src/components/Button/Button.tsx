7c43f802b0f87a3ff2eecc11376c0114e99b2bf1
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-03-24 13:49:20 -0300

    feat: Knowing props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import style from './Button.module.scss';
 
-class Button extends React.Component {
+class Button extends React.Component<{texto: string}> {
   render() {
     return (
       <button className={style.botao}>
-        Botao
+        {this.props.texto}
       </button>
     )
   }
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -13,7 +13,9 @@ class Form extends React.Component {
           <input type="time" step="1" name="tempo" id="tempo" min="00:00:00" max="01:30:00" required />
         </div>
         <div className={style.inputContainer}>
-          <Button/>
+          <Button
+          texto="Adicionar"
+          />
         </div>
       </form>
     )
