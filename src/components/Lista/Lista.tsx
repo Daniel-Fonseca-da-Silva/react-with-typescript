3747c7c26643dabcc60fec5ea30eab94eeab8a7d
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-03-25 03:26:52 -0300

    feat: Using state

--- src/components/Lista/Lista.tsx
+++ src/components/Lista/Lista.tsx
@@ -1,8 +1,9 @@
 import style from './Lista.module.scss';
 import Item from './Item/Item';
+import { useState } from 'react';
 
 function Lista() {
-  const tarefas = [{
+  const [tarefas, setTarefas] = useState([{
     tarefa: 'NextJS',
     tempo: '02:00:00'
   }, {
@@ -11,10 +12,12 @@ function Lista() {
   }, {
     tarefa: 'MongoDB',
     tempo: '12:00:00'
-  }]
+  }]);
   return(
     <aside className={style.listaTarefas}>
-      <h2>Estudos do dia</h2>
+      <h2 onClick={() => {
+        setTarefas([...tarefas, { tarefa: "Estudar estado", tempo: "03:00:00'"}])
+      }}>Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item 
