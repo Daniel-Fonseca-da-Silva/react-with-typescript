83b46d995850d72cfc20ff7a5af579060117bc47
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-03-26 14:33:06 -0300

    feat: Controlling form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,14 +3,25 @@ import Button from "../Button/Button";
 import style from './Form.module.scss';
 
 class Form extends React.Component {
+  state = {
+    tarefa: "",
+    tempo: "00:00"
+  }
+
+  adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+    console.log('State: ', this.state);
+    
+  }
+
   render () {
     return (
-      <form className={style.novaTarefa}>
+      <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo</label>
-          <input type="text" name="tarefa" id="tarefa" placeholder="O que deseja estudar hoje?" required />
+          <input type="text" value={this.state.tarefa} onChange={evento => this.setState({...this.state, tarefa: evento.target.value})} name="tarefa" id="tarefa" placeholder="O que deseja estudar hoje?" required />
           <label htmlFor="tempo">Tempo</label>
-          <input type="time" step="1" name="tempo" id="tempo" min="00:00:00" max="01:30:00" required />
+          <input type="time" step="1" value={this.state.tempo} onChange={evento => this.setState({...this.state, tempo: evento.target.value})} name="tempo" id="tempo" min="00:00:00" max="01:30:00" required />
         </div>
         <div className={style.inputContainer}>
           <Button
