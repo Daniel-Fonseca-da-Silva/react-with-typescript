fd81395b36f2ff83c88b213ed631709a16e93040
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-03-26 23:14:37 -0300

    feat: Updating task from view

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ITarefa } from "../../types/ITarefa";
 import Button from "../Button/Button";
 import style from './Form.module.scss';
+import {v4 as uuidv4} from 'uuid';
 
 class Form extends React.Component<{
   setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
@@ -13,7 +14,21 @@ class Form extends React.Component<{
 
   adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
     evento.preventDefault();
-    this.props.setTarefas(tarefasAntigas => [...tarefasAntigas, {...this.state}])
+    this.props.setTarefas(tarefasAntigas => 
+      [
+        ...tarefasAntigas, 
+      {
+        ...this.state,
+        selecionado: false,
+        completado: false,
+        id: uuidv4()
+      }
+    ]
+    )
+    this.setState({
+      tarefa: "",
+      tempo: "00:00"
+    })
   }
 
   render () {
--- src/components/Lista/Item/Item.tsx
+++ src/components/Lista/Item/Item.tsx
@@ -1,6 +1,9 @@
+import { ITarefa } from '../../../types/ITarefa'
 import style from '../Lista.module.scss'
 
-export default function item({tarefa, tempo}: {tarefa: string, tempo: string}) {
+export default function item({tarefa, tempo, selecionado, completado, id}: ITarefa) {
+  console.log('item atual: ', { tarefa, tempo, selecionado, completado, id });
+  
   return (
     <li className={style.item}>
         <h3>{tarefa}</h3>
