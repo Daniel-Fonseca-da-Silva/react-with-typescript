df21e88f5b8aef6c213ed7da5e491d93a95b70ee
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-03-28 02:23:42 -0300

    feat: Showing the task selected

--- src/components/Lista/Item/Item.tsx
+++ src/components/Lista/Item/Item.tsx
@@ -13,10 +13,12 @@ export default function item({
   id, 
   selecionaTarefa
 }: Props) {
+  console.log('Item atual: ', { tarefa, tempo, selecionado, completado, id });
+  
   return (
-    <li className={style.item} onClick={() => selecionaTarefa({
-        id, tarefa, tempo, selecionado, completado})} 
-      >)
+    <li className={  `${ style.item } ${ selecionado ? style.itemSelecionado : '' }` } onClick={ () => selecionaTarefa({
+        id, tarefa, tempo, selecionado, completado }) } 
+      >
         <h3>{tarefa}</h3>
         <span>{tempo}</span>
     </li>
